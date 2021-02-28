11c6e4c3c55832a1b3606a77e73bc71a12ce626d
Andres Forns Jusino <andresito.jusino@gmail.com>
2021-02-27 22:45:51 -0500

    Revert "Configuracion preliminar de notificaciones 1"
    This reverts commit 28fc4865

--- lib/main.dart
+++ lib/main.dart
@@ -10,66 +10,9 @@ import 'modules/splash/splash_screen.dart';
 import 'data/moor_database.dart';
 import 'ui/screens/onboard.dart';
 
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-
-FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
-final FlutterLocalNotificationsPlugin _flutterLocalNotificationsPlugin =
-    FlutterLocalNotificationsPlugin();
-
-Future<dynamic> myBackgroundMessageHandler(Map<String, dynamic> message) async {
-  if (message.containsKey('data')) {
-    // Handle data message
-    final dynamic data = message['data'];
-  }
-
-  if (message.containsKey('notification')) {
-    // Handle notification message
-    final dynamic notification = message['notification'];
-  }
-  await Firebase.initializeApp();
-
-  // Or do other work.
-}
-
-/// Create a [AndroidNotificationChannel] for heads up notifications
-const AndroidNotificationChannel channel = AndroidNotificationChannel(
-  'high_importance_channel', // id
-  'High Importance Notifications', // title
-  'This channel is used for important notifications.', // description
-  importance: Importance.high,
-);
-
-void main() async {
+void main() {
   SharedPreferencesHelper.wasHereBefore().then((value) =>
       runApp(EasyDynamicThemeWidget(child: App(wasHereBefore: value))));
-  await Firebase.initializeApp();
-
-  _firebaseMessaging.getToken().then((token) {
-    print(token);
-  });
-
-  await _flutterLocalNotificationsPlugin
-      .resolvePlatformSpecificImplementation<
-          AndroidFlutterLocalNotificationsPlugin>()
-      ?.createNotificationChannel(channel);
-
-  _firebaseMessaging.configure(
-    onMessage: (Map<String, dynamic> message) async {
-      print("onMessage: $message");
-      // _showItemDialog(message);
-    },
-    onBackgroundMessage: myBackgroundMessageHandler,
-    onLaunch: (Map<String, dynamic> message) async {
-      print("onLaunch: $message");
-      // _navigateToItemDetail(message);
-    },
-    onResume: (Map<String, dynamic> message) async {
-      print("onResume: $message");
-      // _navigateToItemDetail(message);
-    },
-  );
 }
 
 class App extends StatelessWidget {
