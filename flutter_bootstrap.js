50df3f1a85610b77b5fcc29833f7634629748241
AndresitoStar <andresito.jusino@gmail.com>
2025-08-21 02:43:51 +0000

    Deploying to gh-pages from @ AndresitoStar/movie_search@cacd1f335c79edef9d5f4ca52975178299601ed0 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"871f65ac1bf129edb222c3293a636ff4b67534
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1253203771"
+    serviceWorkerVersion: "3440973633"
   }
 });
